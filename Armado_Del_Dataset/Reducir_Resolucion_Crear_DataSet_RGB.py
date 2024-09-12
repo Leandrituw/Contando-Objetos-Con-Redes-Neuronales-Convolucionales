b8c5de679b6fce0fd4c5ad734892d345456f124d
Rulo <142998718+Leandrituw@users.noreply.github.com>
2024-09-12 16:05:11 -0300

    Reducir_Resolucion_Crear_DataSet_RGB.py

--- Armado_Del_Dataset/Reducir_Resolucion_Crear_DataSet_RGB.py
+++ Armado_Del_Dataset/Reducir_Resolucion_Crear_DataSet_RGB.py
@@ -149,13 +149,13 @@ import numpy as np
 from PIL import Image
 
 # Ruta al directorio del dataset
-dataset_dir = 'D:/Users/Leandro/Downloads/DataSet_1a20_1000_225x400_RGB'
+dataset_dir = 'D:/Users/Leandro/Downloads/Redes Neuronales/DataSets/Maices_Solos/DataSet_1a20_1000_Maices_Solos_225x400_RGB'
 
 # Tamaño de las imágenes
 img_width, img_height = 225, 400
 
-# Obtener las clases del dataset (subcarpetas)
-classes = [d for d in os.listdir(dataset_dir) if os.path.isdir(os.path.join(dataset_dir, d)) and d.isdigit()]
+# Obtener las clases del dataset (subcarpetas) y ordenarlas por su nombre numérico
+classes = sorted([d for d in os.listdir(dataset_dir) if os.path.isdir(os.path.join(dataset_dir, d)) and d.isdigit()], key=int)
 num_classes = len(classes)
 
 # Inicializar listas para almacenar las imágenes y etiquetas
@@ -167,7 +167,7 @@ for class_name in classes:
     class_dir = os.path.join(dataset_dir, class_name)
     # La etiqueta es el nombre de la subcarpeta convertido a entero
     class_idx = int(class_name) - 1  # Restar 1 para que las clases sean de 0 a 19
-    for img_name in os.listdir(class_dir):
+    for img_name in sorted(os.listdir(class_dir)):  # Asegurarse de que las imágenes también estén ordenadas
         img_path = os.path.join(class_dir, img_name)
         img = Image.open(img_path).resize((img_width, img_height))  # Mantener la imagen en RGB
         img_array = np.array(img)
@@ -180,6 +180,12 @@ for class_name in classes:
 # Convertir listas a arrays de NumPy
 images = np.array(images)
 labels = np.array(labels)
+
+# Guardar las imágenes y etiquetas en archivos .npy
+np.save('D:/Users/Leandro/Downloads/Redes Neuronales/DataSets/Maices_Solos/images.npy', images)
+np.save('D:/Users/Leandro/Downloads/Redes Neuronales/DataSets/Maices_Solos/labels.npy', labels)
+
+print("Terminó el proceso")
 
 # Guardar las imágenes y etiquetas en archivos .npy
 np.save('D:/Users/Leandro/Downloads/DataSet_1a20_1000_225x400_RGB/images.npy', images)
